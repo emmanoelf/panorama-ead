4b48448196e9999929efa27627c438e7010f1bd6
Emmanoel F <emmanoel_95@hotmail.com>
2022-03-15 01:33:04 -0300

    refactor: removing unnecessary methods DateProvider

--- src/shared/providers/DateProvider/implementations/DayjsDateProvider.ts
+++ src/shared/providers/DateProvider/implementations/DayjsDateProvider.ts
@@ -6,26 +6,6 @@ import { IDateProvider } from "../IDateProvider";
 dayjs.extend(utc);
 
 class DayjsDateProvider implements IDateProvider {
-    compareInHours(start_date: Date, end_date: Date): number {
-        const end_date_utc = this.convertToUTC(end_date);
-        const start_date_utc = this.convertToUTC(start_date);
-        return dayjs(end_date_utc).diff(start_date_utc, "hours");
-    }
-
-    convertToUTC(date: Date): string {
-        return dayjs(date).utc().local().format();
-    }
-
-    dateNow(): Date {
-        return dayjs().toDate();
-    }
-
-    compareInDays(start_date: Date, end_date: Date): number {
-        const end_date_utc = this.convertToUTC(end_date);
-        const start_date_utc = this.convertToUTC(start_date);
-        return dayjs(end_date_utc).diff(start_date_utc, "days");
-    }
-
     addDays(days: number): Date {
         return dayjs().add(days, "days").toDate();
     }
@@ -34,8 +14,12 @@ class DayjsDateProvider implements IDateProvider {
         return dayjs().add(hours, "hour").toDate();
     }
 
-    compareIfBefore(start_date: Date, end_date: Date): boolean {
+    isResetPasswordTimeExpired(start_date: Date, end_date: Date): boolean {
         return dayjs(start_date).isBefore(end_date);
+    }
+
+    dateNow(): Date {
+        return dayjs().toDate();
     }
 }
 
